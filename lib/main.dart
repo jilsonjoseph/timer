238e3251baf5d2588ca0f49e48e612e15d1937a5
Jilson <4jilson@gmail.com>
2018-12-19 09:56:08 +0530

    UI blocked out

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,42 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: Container(),
+      home: Scaffold(body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: double.infinity,
+              height: 150,
+            ),
+            Container(
+              color: Colors.blue,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: AspectRatio(
+                    aspectRatio: 1.0,
+                    child: Container(
+                      color: Colors.amberAccent,
+                      width: double.infinity,
+                    ),
+                ),
+              )
+            ),
+            Expanded(child: Container()),
+            Container(
+              color: Colors.black38,
+              width: double.infinity,
+              height: 50,
+            ),
+            Container(
+              color: Colors.deepPurple,
+              width: double.infinity,
+              height: 50,
+            ),
+          ],
+        ),
+      ),
       ),
     );
   }
