136b43673b154d1936f25b94d371b6be4466ef93
Jilson <4jilson@gmail.com>
2019-02-03 14:58:36 +0530

    Ticks drawing completed

--- lib/timer_dial.dart
+++ lib/timer_dial.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timer/timer_knob.dart';
+import "dart:math";
 
 final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
 final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
@@ -35,14 +36,77 @@ class _TimerDialState extends State<TimerDial> {
                     )
                   ]
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(65.0),
-                child: TimerKnob(),
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    padding: const EdgeInsets.all(55.0),
+                    child: CustomPaint(
+                      painter: TickPainter(
+
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(65.0),
+                    child: TimerKnob(),
+                  ),
+                ]
               ),
             ),
           ),
         )
     );
   }
+}
+
+class TickPainter extends CustomPainter{
+
+  final LONG_TICK = 14.0;
+  final SHORT_TICK = 4.0;
+
+  final tickCount;
+  final ticksPerSection;
+  final tickInset;
+  final tickPaint;
+
+  TickPainter({
+    this.tickCount = 35,
+    this.ticksPerSection = 5,
+    this.tickInset = 0,
+}) : tickPaint = new Paint() {
+    tickPaint.color = Colors.black;
+    tickPaint.strokeWidth = 1.5;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.translate(size.width/2, size.height/2);
+
+    canvas.save();
+
+    final radius = size.width/2;
+
+    for(var i = 0; i< tickCount; ++i){
+      final tickLength = i % ticksPerSection == 0 ? LONG_TICK : SHORT_TICK;
+
+      canvas.drawLine(
+          Offset(0.0, -radius),
+          Offset(0.0, -radius -tickLength),
+          tickPaint
+      );
+
+      canvas.rotate(2*pi / tickCount);
+    }
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
 }
 
