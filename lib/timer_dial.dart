5788db2a00aeed7e0cd44489b3ded7d49ed81475
Jilson <4jilson@gmail.com>
2019-02-03 14:14:28 +0530

    Arrow Painted to Knob

--- lib/timer_dial.dart
+++ lib/timer_dial.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timer/timer_knob.dart';
 
 final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
 final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
@@ -36,47 +37,12 @@ class _TimerDialState extends State<TimerDial> {
               ),
               child: Padding(
                 padding: const EdgeInsets.all(65.0),
-                child: Container(
-                  padding: const EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [GRADIENT_TOP,GRADIENT_BOTTOM],
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: const Color(0x44000000),
-                          blurRadius: 2.0,
-                          spreadRadius: 1.0,
-                          offset: const Offset(0.0, 1.0),
-                        )
-                      ]
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: const Color(0xFFDFDFDF),
-                          width: 1.5,
-                        )
-
-                    ),
-                    child: new Center(
-                      child: new Image.asset('assets/icons_circled_j.png',
-                        width: 50,
-                        height: 50,
-                      ),
-                    ),
-                  ),
-                ),
+                child: TimerKnob(),
               ),
-
-
             ),
           ),
         )
     );
   }
 }
+
