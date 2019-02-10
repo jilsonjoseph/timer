846df918d02bab66dd879c5388e94f44ceb878d2
Jilson <4jilson@gmail.com>
2019-02-10 13:19:11 +0530

    pause resume, restart, reset buttons animation complete

--- lib/timer_controls.dart
+++ lib/timer_controls.dart
@@ -22,14 +22,61 @@ class TimerControls extends StatefulWidget {
   _TimerControlsState createState() => _TimerControlsState();
 }
 
-class _TimerControlsState extends State<TimerControls> {
+class _TimerControlsState extends State<TimerControls> with TickerProviderStateMixin{
+
+  AnimationController pauseResumeSlideController;
+  AnimationController restartResetFadeController;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    pauseResumeSlideController =  AnimationController(
+        duration: const Duration(milliseconds: 150),
+        vsync: this
+    )
+      ..addListener(() => setState((){}));
+    pauseResumeSlideController.value = 1.0;
+
+    restartResetFadeController =  AnimationController(
+        duration: const Duration(milliseconds: 150),
+        vsync: this
+    )
+      ..addListener(() => setState((){}));
+    restartResetFadeController.value = 1.0;
+  }
+
+
+  @override
+  void dispose() {
+    pauseResumeSlideController.dispose();
+    restartResetFadeController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    switch(widget.timerState){
+      case TimerState.ready:
+        pauseResumeSlideController.forward();
+        restartResetFadeController.forward();
+        break;
+      case TimerState.running:
+        pauseResumeSlideController.reverse();
+        restartResetFadeController.forward();
+        break;
+      case TimerState.paused:
+        pauseResumeSlideController.reverse();
+        restartResetFadeController.reverse();
+        break;
+    }
+
     return Column(
       children: [
         Opacity(
-          opacity: 1.0,
+          opacity: 1.0 - restartResetFadeController.value,
           child: Row(
             children: [
               TimerButton(
@@ -47,7 +94,10 @@ class _TimerControlsState extends State<TimerControls> {
           ),
         ),
         Transform(
-          transform: Matrix4.translationValues(0.0,0.0,0.0),
+          transform: Matrix4.translationValues(
+              0.0,
+              100.0 * pauseResumeSlideController.value ,
+              0.0),
           child: TimerButton(
             icon:widget.timerState == TimerState.running ? Icons.pause : Icons.play_arrow,
             text:widget.timerState == TimerState.running ? 'PAUSE' : 'RESUME',
